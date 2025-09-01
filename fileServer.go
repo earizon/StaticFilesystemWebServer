ae9a8ac08ec57359c0347dcdb7ec753d47cb3c84
Enrique Arizón Benito <enrique.arizon@accenture.com>
2025-09-01 23:14:15 +0200

    Parameterize host/port/work dir

--- fileServer.go
+++ fileServer.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-    "net/http"
-    "log"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/go-http-utils/etag"
 )
 
 func main() {
-    // Serve files from the "static" directory
-    fileServer := http.FileServer(http.Dir(".."))
-    
-    // Strip the "/static/" prefix when looking for files
-    http.Handle("/", etag.Handler(fileServer, false)) //http.StripPrefix("/static", fileServer))
+	// Once all flags are declared, call `flag.Parse()`
+	// to execute the command-line parsing.
+	var h = flag.String("host", "0.0.0.0", "address to listen on. default value: 0.0.0.0 -all network interfaces-")
+	var p = flag.Int("port", 7777, "tcp port to listen on. default value: 7777")
+	var w = flag.String("dir", "/error", "directory with static files to serve")
+	flag.Parse()
+	// err := os.Chdir(workDir)
+	// if err != nil {
+	//     panic(err)
+	// }
+	fileServer := http.FileServer(http.Dir(*w))
 
-    // Start the server on port 7777
-    log.Println("Starting server on :7777")
-    if err := http.ListenAndServe(":7777", nil); err != nil {
-        log.Fatal(err)
-    }
+	// Strip the "/static/" prefix when looking for files
+	http.Handle("/", etag.Handler(fileServer, false)) //http.StripPrefix("/static", fileServer))
+
+	// Start the server on port 7777
+	log.Printf("Starting server on %s:%d serving files in dir: %s", *h, *p, *w)
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", *h, *p), nil); err != nil {
+		panic(err)
+	}
 }
